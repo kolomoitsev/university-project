f2a3937c53f3cc1045aacd90d26b5c1a7e212c3c
Vladimir Mozgin <mozgin.vova@gotoinc.co>
2020-09-20 15:35:47 +0300

    Create canvas editor & fields validation

--- src/pages/label.img.page.js
+++ src/pages/label.img.page.js
@@ -11,9 +11,21 @@ class LabelImg extends React.Component {
             ctx: null,
             x: null,
             y: null,
+            tmpX: null,
+            tmpY: null,
             isDrawing: false,
             rectangles: [],
-            img: null
+            img: null,
+            ctxWidth: 0,
+            ctxHeight: 0,
+            XzoomCoefficient: 0,
+            YzoomCoefficient: 0,
+            ctxBorderColor: props.color || 'green',
+            ctxBackColor: props.color || 'black',
+            ctxFontColor: props.color || 'white',
+            ctxBorder: props.border || 5,
+            coef: 1,
+            error: ''
         };
     }
 
@@ -22,45 +34,60 @@ class LabelImg extends React.Component {
     }
 
     initializeCanvas = () => {
-        const ctx = this.refs.canvas.getContext('2d')
+        let ctx = this.refs.canvas.getContext('2d')
         const img = this.refs.image
+        const canvasWrapper = this.refs.canvasWrapper
+        let windowWidth = window.innerWidth
+        let heightOffset = this.getOffset(canvasWrapper)
+
+        ctx.font = "30px Arial";
 
         img.onload = () => {
+            let coef = img.width / windowWidth
+            let ctxHeight = img.height / coef
+
             this.setState((state) => ({
                 ...state,
                 ctx,
-                img
+                img,
+                coef,
+                ctxWidth: windowWidth,
+                ctxHeight,
+                YzoomCoefficient: heightOffset
             }))
-
-            this.state.ctx.drawImage(img, 0, 0)
         }
     }
 
     setPoint = (event) => {
-        console.log(event)
-
         if (this.state.isDrawing) {
-            let x = event.pageX - 76, y = event.pageY - 2,
-                width = x - this.state.x, height = y - this.state.y
+            let x = event.pageX - this.state.XzoomCoefficient, y = event.pageY - this.state.YzoomCoefficient
+            let { width, height } = this.calcWidthAndHeight(this.state.x, x, this.state.y, y)
+            let id = this.state.rectangles.length + 1
 
-            console.log(this.state.x, width)
-            this.state.ctx.clearRect(
-                this.state.x,
-                this.state.y,
+            this.drawRectangle(this.state.x, this.state.y, width, height, id)
+
+            let rectangles = this.state.rectangles
+            rectangles.push({
+                id,
+                x: this.state.x,
+                y: this.state.y,
                 width,
-                height
-            );
-
-            this.state.ctx.fillRect(this.state.x, this.state.y, width, height);
+                height,
+                label: '',
+                regex: ''
+            })
 
             this.setState((state) => ({
                 ...state,
                 x: null,
                 y: null,
-                isDrawing: !state.isDrawing
+                tmpX: null,
+                tmpY: null,
+                isDrawing: !state.isDrawing,
+                rectangles
             }))
         } else {            
-            let x = event.pageX - 76, y = event.pageY - 2;
+            let x = event.pageX - this.state.XzoomCoefficient, y = event.pageY - this.state.YzoomCoefficient;
         
             this.setState((state) => ({
                 ...state,
@@ -69,53 +96,192 @@ class LabelImg extends React.Component {
                 isDrawing: !state.isDrawing
             }))
 
-            this.state.ctx.fillStyle = "green";
+            this.state.ctx.fillStyle = this.state.ctxBorderColor;
             this.state.ctx.fillRect(x,y,10,10);
         }
     }
 
-    reDrawImg = () => {
-        let newCtx = this.state.ctx
-        newCtx.globalCompositeOperation = 'existing content'
+    drawRectangle(x, y, width, height, text) {
+        // Clear aria before
+        this.state.ctx.clearRect(
+            x,
+            y,
+            width,
+            height
+        );
+
+        const border = this.state.ctxBorder
+        this.state.ctx.fillRect(x, y, width, height);
+        this.state.ctx.clearRect(x + border, y + border, width - (border * 2), height - (border * 2));
+        
+        let ctx = this.state.ctx
+        ctx.globalAlpha = 0.2;
+        ctx.fillStyle = this.state.ctxBackColor
+        this.setState((state) => ({
+            ...state,
+            ctx
+        }))
+        ctx.fillRect(x + border, y + border, width - (border * 2), height - (border * 2));
+
+        const prevColor = this.state.ctxBorderColor;
+
+        ctx.globalAlpha = 1.0;
+        ctx.font = "40px Arial";
+        ctx.fillStyle = this.state.ctxFontColor;
 
         this.setState((state) => ({
             ...state,
-            ctx: newCtx
+            ctx
         }))
 
-        this.state.ctx.drawImage(this.state.img, 0, 0)
+        this.state.ctx.fillText(`${text}`, x + Math.round(width / 2) - 10, y + Math.round(height / 2) + 7);
 
-        newCtx = this.state.ctx
-        this.state.ctx.globalCompositeOperation = 'new content'
-
+        ctx.fillStyle = prevColor;
         this.setState((state) => ({
             ...state,
-            ctx: newCtx
+            ctx
         }))
     }
 
-    drawBorder = (event) => {
-        // console.log(event)
-        // if (this.state.isDrawing) {
-        //     console.log('Drawing')
-        //     this.state.ctx.beginPath();
-        //     this.state.ctx.moveTo(0, 0);
-        //     this.state.ctx.lineTo(300, 150);
-        //     this.state.ctx.stroke();
-        // }
+    omPointerMove = (event) => {
+        if (!this.state.isDrawing) {
+            return
+        }
+        let tmpX = event.pageX - this.state.XzoomCoefficient, tmpY = event.pageY - this.state.YzoomCoefficient
+        
+        if (this.state.tmpX && this.state.tmpY) {
+            let { width, height } = this.calcWidthAndHeight(this.state.x, this.state.tmpX, this.state.y, this.state.tmpY)
+
+            this.state.ctx.clearRect(
+                this.state.x,
+                this.state.y,
+                width + this.state.ctxBorder,
+                height + this.state.ctxBorder
+            );
+        }
+
+        this.setState((state) => ({
+            ...state,
+            tmpX,
+            tmpY
+        }))
+        
+        this.drawBorder(this.state.x, this.state.y, tmpX, tmpY)
+    }
+
+    calcWidthAndHeight(x1, x2, y1, y2) {
+        let width = Math.abs(x1 - x2), height = Math.abs(y1 - y2)
+        return {
+            width,
+            height
+        }
+    }
+
+    drawBorder = (x1, y1, x2, y2) => {
+        let { width, height } = this.calcWidthAndHeight(x1, x2, y1, y2)
+        let border = this.state.ctxBorder, borderX2 = border * 2
+
+        if (this.state.isDrawing) {
+            this.state.ctx.fillRect(x1, y1, width, height);
+            this.state.ctx.clearRect(x1 + border, y1 + border, width - borderX2, height - borderX2);
+        }
+    }
+
+    getOffset(el) {
+        let y = 0;
+
+        while(el != null) {
+            y += el.offsetTop;
+            el = el.offsetParent;
+        }
+
+        return y;
+    }
+
+    handleInput = (event) => {
+        const input  = event.target,
+            value = input.value,
+            type = input.dataset.type,
+            id = input.dataset.id
+
+        const recIndex = this.state.rectangles.findIndex(rec => rec.id == id)
+
+        if (recIndex !== -1) {
+            const updatedRectangle = this.state.rectangles[recIndex]
+            updatedRectangle[type] = value
+
+            const rectangles = this.state.rectangles
+            rectangles[recIndex] = updatedRectangle
+
+            this.setState((state) => ({
+                ...state,
+                rectangles
+            }))
+        }
+    }
+
+    validateInputs = () => {
+        const validRectangles = this.state.rectangles.filter(rec => {
+            return rec.label.length >= 3
+        })
+
+        return validRectangles.length === this.state.rectangles.length
+    }
+
+    handleSubmit = () => {
+        const isValid = this.validateInputs()
+        
+        if (isValid) {
+
+        } else {
+            this.setState((state) => ({
+                ...state,
+                error: 'All labels are required!'
+            }))
+        }
     }
 
     render() {
         return (
-            <div className="canvas-wrapper">
-                <canvas
-                    ref="canvas"
-                    onClick={this.setPoint}
-                    width={1200} height={900}
-                    style={{ background: '../assets/img/test_img.png' }}
-                    onPointerMove={this.drawBorder}
-                 />
-                <img ref="image" src={TestImg} className="d-none" />
+            <div>
+                <div className="canvas-wrapper" ref="canvasWrapper">
+                    <canvas
+                        ref="canvas"
+                        onClick={this.setPoint}
+                        width={this.state.ctxWidth} height={this.state.ctxHeight}
+                        style={{ background: '../assets/img/test_img.png' }}
+                        onPointerMove={this.omPointerMove}
+                    />
+                    <img ref="image" src={TestImg} className="d-none" />
+                </div>
+                <div className="row m-0">
+                    { this.state.rectangles.map(rec => {
+                        return <div key={rec.id} className="col-md-3">
+                                    <form>
+                                        <div className="form-group">
+                                        <h4>Aria #{rec.id}</h4>
+                                        <label>Label (required)</label>
+                                        <input type="text" onChange={this.handleInput} className="form-control" data-id={`${rec.id}`} data-type="label" placeholder="Field 1"></input>
+                                        <label>Regex (optional)</label>
+                                        <input type="text" onChange={this.handleInput} className="form-control" data-id={`${rec.id}`} data-type="regex" placeholder="%^abc%"></input>
+                                        <small className="form-text text-muted">Regular expression for the data</small>
+                                        </div>
+                                    </form>
+                                </div>
+                    }) }
+                </div>
+                { this.state.rectangles.length > 0 && 
+                    <div className="row m-0">
+                        { this.state.error &&
+                            <div className="alert alert-danger text-center col-10 offset-1 mb-2 mt-2" role="alert">
+                                {this.state.error}
+                            </div>
+                        }
+                        <div className="col-md-2 text-center offset-md-5 mb-4 mt-2">
+                            <button type="button" className="btn btn-success" onClick={this.handleSubmit}>Finish</button>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
