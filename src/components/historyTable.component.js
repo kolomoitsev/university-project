47dba0f2da3d55d86bed5bf665a5287343626365
Pavlo Kolomoitsev <pavlo.kolomoitsev@nure.ua>
2020-12-15 22:53:40 +0200

    done.

--- src/components/historyTable.component.js
+++ src/components/historyTable.component.js
@@ -17,16 +17,9 @@ const HistoryTable = () => {
 
     const [foundedData, setFoundedData] = useState(null)
 
-    function search(arr, s){
-        let matches = [], i, key;
+    const [templates, setTemplates] = useState(null)
 
-        for( i = arr.length; i--; )
-            for( key in arr[i] )
-                if( arr[i].hasOwnProperty(key) && arr[i][key].indexOf(s) > -1 )
-                    matches.push( arr[i] );  // <-- This can be changed to anything
-
-        return matches;
-    };
+    const [selectedCheckBoxes, setSelectedCheckBoxes] = useState([])
 
     useEffect( () => {
 
@@ -61,18 +54,84 @@ const HistoryTable = () => {
 
     }, [selectedItem])
 
+    const getJsonFile = (data) => {
+        const jsonF = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data.parsed_data));
+        return <a class="btn customBtn" href={`data:' + ${jsonF} + '`} download="data.json">download JSON</a>;
+    }
+
+    useEffect( () => {
+
+        const getTemplates = async () => {
+
+            const { data : { results } } = await axios.get(`${process.env.REACT_APP_API_SERVER}/structures/`, {
+                headers: {
+                    'Authorization' : `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .catch(err => console.log(err))
+
+            //results && console.log(results)
+
+            results && setTemplates(results)
+
+        }
+
+        getTemplates()
+
+    }, [])
+
     const filterIt = (arr, searchKey) =>  {
-        
+
+        if(!searchKey) return arr
+
+        return arr.filter(item => {
+            const finalResult = item.parsed_data.find(singleItem => {
+                const keys = Object.keys(singleItem)
+                const values = Object.values(singleItem)
+                const resultArray = [...keys, ...values]
+                const result = resultArray.find(t => t.toString().includes(searchKey))
+                return !!result
+            })
+
+            return !!finalResult
+        })
+
     }
 
     const handleSearchClick = () => {
-        history && console.log(history)
-        history && console.log(filterIt(history, searchItem))
+        //history && console.log(history)
+        history && setFoundedData(filterIt(history, searchItem))
     }
 
     const handleFormClick = (i) => {
         setSelectedItem(i)
     }
+
+    useEffect(() => {
+
+        if(!selectedCheckBoxes.length){
+            return setFoundedData(null)
+        }
+
+        const result = history.filter(x => selectedCheckBoxes.some(t => t === x.template))
+
+        setFoundedData(result)
+
+    }, [selectedCheckBoxes])
+
+
+
+    const handleCheckboxChange = async (event) => {
+
+        let newArray = [...selectedCheckBoxes, event.target.value];
+
+        if (selectedCheckBoxes.includes(event.target.value)) {
+            newArray = newArray.filter(day => day !== event.target.value);
+        }
+
+        setSelectedCheckBoxes(newArray)
+
+    };
 
     const sortBy = async (field) => {
 
@@ -93,8 +152,6 @@ const HistoryTable = () => {
         //history && console.log(history)
     }
 
-
-
     return  (
         <>
 
@@ -105,7 +162,7 @@ const HistoryTable = () => {
                 </div>
             </div> }
 
-            <table className="table table-striped table-bordered">
+            { <table className="table table-striped table-bordered">
                 <thead>
                 <tr className="center-col">
                     <th scope="col"><a onClick={() => sortBy('id')}>Index</a></th>
@@ -118,39 +175,6 @@ const HistoryTable = () => {
                 </thead>
                 <tbody>
 
-
-                { history && history.map((item, index) =>
-                    <tr className="centeredItem">
-                        <td>{ index + 1 }</td>
-                        <td>{ item.template_name }</td>
-                        <td>en</td>
-                        <td>{ new Date(item.updated).toLocaleString() }</td>
-                        <td><a onClick={event => handleFormClick(index+1)} className="btn customBtn">Show</a></td>
-                        <td id={`place-${index}`}> </td>
-                    </tr>
-                    )
-                 }
-
-
-                </tbody>
-            </table>
-
-            <input onChange={event => setSearchItem(event.target.value)} type="text" placeholder={"Input parsed data value to search"}/>
-            <button onClick={() => handleSearchClick()} >Find</button>
-
-            { foundedData && <table className="table table-striped table-bordered">
-                <thead>
-                <tr className="center-col">
-                    <th scope="col"><a onClick={() => sortBy('id')}>Index</a></th>
-                    <th scope="col"><a onClick={() => sortBy('template_name')} >Template name</a></th>
-                    <th scope="col">Document language</th>
-                    <th scope="col"><a onClick={() => sortBy('created')}>Document date</a></th>
-                    <th scope="col">More data</th>
-                </tr>
-                </thead>
-                <tbody>
-
-
                 { foundedData && foundedData.map((item, index) =>
                     <tr className="centeredItem">
                         <td>{ index + 1 }</td>
@@ -158,13 +182,47 @@ const HistoryTable = () => {
                         <td>en</td>
                         <td>{ new Date(item.updated).toLocaleString() }</td>
                         <td><a onClick={event => handleFormClick(index+1)} className="btn customBtn">Show</a></td>
+                        <td id={`place-${index}`}>{getJsonFile(item)}</td>
                     </tr>
                 )
                 }
 
+                { (!foundedData && history) && history.map((item, index) =>
+                    <tr className="centeredItem">
+                        <td>{ index + 1 }</td>
+                        <td>{ item.template_name }</td>
+                        <td>en</td>
+                        <td>{ new Date(item.updated).toLocaleString() }</td>
+                        <td><a onClick={event => handleFormClick(index+1)} className="btn customBtn">Show</a></td>
+                        <td id={`place-${index}`}>{getJsonFile(item)}</td>
+                    </tr>
+                    )
+                 }
+
 
                 </tbody>
             </table> }
+
+            <div className="groupSearch">
+                <input className="searchInput" onChange={event => setSearchItem(event.target.value)} type="text" placeholder={"Input parsed data value to search"}/>
+                <button className="btn" onClick={() => handleSearchClick()} >Find</button>
+
+            </div>
+
+            <div className="checkFilter">
+                <h5>Filter by template name</h5>
+                { templates && templates.map((item, index) =>
+                    <div className="checkItem">
+                        <input onChange={event => handleCheckboxChange(event)} type="checkbox" id={`template${index}`} name={`template${index}`} value={item.id} />
+                        <label htmlFor={`template${index}`}>{item.name}</label>
+                    </div>
+                ) }
+            </div>
+
+
+
+
+
 
         </>
     )
